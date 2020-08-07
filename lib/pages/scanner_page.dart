875f14074a2ed7092f483fc474f952e63e65750e
joe02 <joe02araujo@gmail.com>
2020-08-07 07:47:51 -0300

    First commit.

---
+++ lib/pages/scanner_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class ScannerPage extends StatefulWidget {
+  @override
+  ScannerPageState createState() => ScannerPageState();
+}
+
+class ScannerPageState extends State<ScannerPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
