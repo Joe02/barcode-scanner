77fda4f9c887837793c135704ee9f8d2b0b93991
joe02 <joe02araujo@gmail.com>
2020-08-07 08:31:24 -0300

    Added Barcode scanner to the App.

--- lib/pages/scanner_page.dart
+++ lib/pages/scanner_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 
 class ScannerPage extends StatefulWidget {
   @override
@@ -7,10 +9,81 @@ class ScannerPage extends StatefulWidget {
 }
 
 class ScannerPageState extends State<ScannerPage> {
+  String _scanBarcode = '';
+
+  startBarcodeScanStream() async {
+    FlutterBarcodeScanner.getBarcodeStreamReceiver(
+            "#ff6666", "Cancel", true, ScanMode.BARCODE)
+        .listen((barcode) => _scanBarcode = barcode);
+  }
+
+  Future<void> scanQR() async {
+    String barcodeScanRes;
+    try {
+      barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
+          "#ff6666", "Cancel", true, ScanMode.QR);
+      print(barcodeScanRes);
+    } on PlatformException {
+      barcodeScanRes = 'Failed to get platform version.';
+    }
+    if (!mounted) return;
+
+    setState(() {
+      _scanBarcode = barcodeScanRes;
+    });
+  }
+
+  Future<void> scanBarcodeNormal() async {
+    String barcodeScanResult;
+    try {
+      barcodeScanResult = await FlutterBarcodeScanner.scanBarcode(
+          "#ff6666", "Cancel", true, ScanMode.BARCODE);
+      print(barcodeScanResult);
+    } on PlatformException {
+      barcodeScanResult = 'Failed to get platform version.';
+    }
+    if (!mounted) return;
+
+    setState(() {
+      _scanBarcode = barcodeScanResult;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-
+    return MaterialApp(
+      home: Scaffold(
+        body: Builder(
+          builder: (BuildContext context) {
+            return Container(
+              alignment: Alignment.center,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  FlatButton(
+                    child: Text("Iniciar stream de reconhecimento"),
+                    onPressed: () {
+                      startBarcodeScanStream();
+                    },
+                  ),
+                  SizedBox(
+                    width: 10,
+                    height: 10,
+                  ),
+                  FlatButton(
+                    child: Text("Iniciar reconhecimento para QR Code"),
+                    onPressed: () {
+                      scanQR();
+                    },
+                  ),
+                  Text('Resultado : $_scanBarcode\n',
+                      style: TextStyle(fontSize: 20))
+                ],
+              ),
+            );
+          },
+        ),
+      ),
     );
   }
 }
